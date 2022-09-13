9d2905cd1baaf929d1682b34a9862c6679cd0998
ssalpo <ssalpomishevich@gmail.com>
2022-09-13 15:46:41 +0500

    фикс роутинга ресайзера фотографий

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ Route::middleware('auth:api')->group(function () {
 
     Route::post('files/upload', 'FileController@upload');
     Route::get('files/{folder}/{filename}', 'FileController@file');
+    Route::get('files/image/{filename}/{width}/{height}', 'FileController@image');
 
     Route::apiResource('products', 'ProductController');
 });
-Route::get('files/image/{filename}/{width}/{height}', 'FileController@image');
