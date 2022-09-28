44f6839708d60f485747127451f24c6d699aaa5a
ssalpo <ssalpomishevich@gmail.com>
2022-09-28 10:03:24 +0500

    убрал добавление тестовых данных, добавил тестовых администраторов

---
+++ database/seeders/DataForTestingSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class DataForTestingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(RolesTableSeeder::class);
+
+        User::factory(10)->create()->each(function (User $user) {
+            $user->assignRole(Role::inRandomOrder()->first());
+        });
+
+        \App\Models\Client::factory(10)->create();
+
+        Product::factory(10)->create();
+    }
+}
